51b34fc1148795ea3ebec6ec57e72c1d2499c6ee
João Rainier Carvalho <118614112+joaorainier@users.noreply.github.com>
2024-04-21 19:15:39 -0300

    Add files via upload

---
+++ Nível 1 - Vamos Criar um App/App.js
@@ -0,0 +1,60 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+import ListaFornecedores from './components/ListaFornecedores';
+import { useEffect, useState } from 'react';
+
+export default function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    getData().then((newData) => {
+      if (!newData || JSON.stringify(newData) === '{}' || JSON.stringify(newData) === '[]') {
+        newData = [
+          { id: 1, nome: "Pessoa 1", endereco: "endereco", contato: "contato", categoria: "categoria" },
+          { id: 2, nome: "Teste Novo", endereco: "SQN 311 Bloco C", contato: "112233445", categoria: "teste" }];
+      }
+      setData(newData);
+    });
+  }, [])
+
+  const atualizarFornecedores = (fornecedores) => {
+    setData(fornecedores)
+    storeData(fornecedores)
+  }
+
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <ListaFornecedores fornecedores={data} atualizarFornecedores={atualizarFornecedores}></ListaFornecedores>
+    </View>
+  );
+}
+
+const storeData = async (value) => {
+  try {
+    const jsonValue = JSON.stringify(value);
+    await AsyncStorage.setItem('my-key', jsonValue);
+  } catch (e) {
+    // saving error
+  }
+};
+
+const getData = async () => {
+  try {
+    const jsonValue = await AsyncStorage.getItem('my-key');
+    return jsonValue != null ? JSON.parse(jsonValue) : null;
+  } catch (e) {
+    // error reading value
+  }
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingBottom: 50
+  },
+});
